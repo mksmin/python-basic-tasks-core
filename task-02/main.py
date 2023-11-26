8072c885eedf0bf61a48bd39aa9154f012414411
Suren Khorenyan <surenkhorenyan@gmail.com>
2023-11-27 01:06:35 +0300

    Add task 02 core

---
+++ task-02/main.py
@@ -0,0 +1,64 @@
+# Самостоятельная работа №2
+
+# Задача №1. Конвертация верблюда в змею
+
+def camel_case_to_snake_case(line: str) -> str:
+    ...
+
+
+# Задача №2. Повторение
+
+class Person:
+    ...
+
+
+# Задача №3. Неабстрактные формы
+
+class Shape:
+    ...
+
+
+class Rectangle:
+    ...
+
+
+class Circle:
+    ...
+
+
+# Задача №4. Ошибка суммы
+
+def add():
+    ...
+
+
+# Задача №5*. Габариты экрана
+
+class Screen:
+
+    def aspect_ratio():
+        ...
+
+    def height():
+        ...
+
+    def width():
+        ...
+
+    def height_cm():
+        ...
+
+    def width_cm():
+        ...
+
+    def area():
+        ...
+
+    def area_cm():
+        ...
+
+
+# Задача №6*. Валидные скобки
+
+def valid_parentheses():
+    ...
