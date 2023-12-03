57a2d5c83179ee50ae3d9e65f06a725859cb7426
Suren Khorenyan <surenkhorenyan@gmail.com>
2023-12-03 22:12:35 +0300

    Add task 06 core

---
+++ task-06/crud.py
@@ -0,0 +1,1 @@
+API_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/{date}/currencies/{from_currency}/{to_currency}.json"
---
+++ task-06/main.py
@@ -0,0 +1,7 @@
+# Самостоятельная работа №6
+
+import uvicorn
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
